4a50981e76aaf43f8cc0701edee7dc1c2928334e
Robin Biscay <Biscay.rob@gmail.com>
2021-02-04 00:29:45 +0100

    Added category selection.

--- sketch.js
+++ sketch.js
@@ -4,7 +4,25 @@ function changeQuestion() {
         let ctgrElmt = document.getElementById("sf1-select");
         let category = ctgrElmt.options[ctgrElmt.selectedIndex].text;
         let ctnElmt = document.getElementById("quotation-slot");
+
         current_idx = Math.floor(Math.random() * enigmes.length);
-        ctnElmt.innerHTML = enigmes[current_idx][0];
-        ctnElmt.cite= enigmes[current_idx][1];
+        let usedArray = enigmes;
+        switch(category) {
+          case 'Enigmes':
+          break;
+          case 'Décimaux':
+          usedArray = decimaux;
+          break;
+          case 'Fractions':
+          usedArray = fractions;
+          break;
+          case 'Décimaux et fractions':
+          usedArray = fractionsDecimaux
+          break;
+          default:
+            return;
+        }
+
+        ctnElmt.innerHTML = usedArray[current_idx][0];
+        ctnElmt.cite = usedArray[current_idx][1];
 }
